bc888b6cf49d610ad06959148a2af11f86302c89
Mathis Neunzig <mathis.neunzig@gmail.com>
2025-06-04 13:29:00 +0200

    first version

---
+++ src/components/Resume.js
@@ -0,0 +1,164 @@
+import React, { Component } from 'react';
+export default  class Resume extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="resume">
+
+         <div className="row education">
+
+            <div className="three columns header-col">
+               <h1><span>Education</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                resumeData.education && resumeData.education.map((item)=>{
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.UniversityName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <br></br>
+                          <em className="date">({item.Achievements})</em>
+                          <br></br>
+                          <em className="date">{item.MonthOfStarting} {item.YearOfStarting} - {item.MonthOfPassing} {item.YearOfPassing}</em></p>
+                       </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+         </div>
+
+        <div className="row education">
+
+          <div className="three columns header-col">
+              <h1><span>Certificates</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            {
+              resumeData.certificates && resumeData.certificates.map((item)=>{
+                return(
+                  <div className="row item">
+                      <div className="twelve columns">
+                        <h3>{item.UniversityName}</h3>
+                        <p className="info">
+                        {item.specialization}
+                        <br></br>
+                        <em className="date">({item.Achievements})</em>
+                        <br></br>
+                        <em className="date">{item.MonthOfStarting} {item.YearOfStarting} - {item.MonthOfPassing} {item.YearOfPassing}</em></p>
+                      </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+        <div className="row work">
+            <div className="three columns header-col">
+               <h1><span>Work</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                resumeData.work && resumeData.work.map((item) => {
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.CompanyName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <br></br>
+                          {(item.YearOfLeaving != undefined || item.YearOfLeaving != null) ? <em className="date">{item.MonthOfStarting} {item.YearOfStarting} - {item.MonthOfLeaving} {item.YearOfLeaving}</em> : <em className="date">From {item.MonthOfStarting} {item.YearOfStarting}</em>}
+                          </p><p>
+                          {item.Achievements}
+                          </p>
+                       </div>
+
+                    </div>
+
+                  )
+                })
+              }
+            </div> 
+         </div>
+        <div className="row work">
+            <div className="three columns header-col">
+               <h1><span>Teaching</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                resumeData.teaching && resumeData.teaching.map((item) => {
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.CompanyName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <br></br>
+                          {(item.YearOfLeaving != undefined || item.YearOfLeaving != null) ? <em className="date">{item.MonthOfStarting} {item.YearOfStarting} - {item.MonthOfLeaving} {item.YearOfLeaving}</em> : <em className="date">From {item.MonthOfStarting} {item.YearOfStarting}</em>}
+                          </p><p>
+                            <ul>
+                              {item.Modules && item.Modules.map((module) => {
+                                return (
+                                    <li>
+                                      - {module.Name} ({module.ECTS} ECTS)
+                                    </li>
+                                )
+                              })}
+                            </ul>
+                          </p>
+                       </div>
+
+                    </div>
+
+                  )
+                })
+              }
+            </div> 
+         </div>
+
+
+         {/* <div className="row skill">
+
+            <div className="three columns header-col">
+               <h1><span>Skills</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+
+               <p>
+               {resumeData.skillsDescription}
+               </p>
+
+   				<div className="bars">
+
+   				   <ul className="skills">
+                {
+                  resumeData.skills && resumeData.skills.map((item) => {
+                    return(
+                      <li>
+                      <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
+                      </span><em>{item.skillname}</em>
+                      </li>
+                    )
+                  })
+                }
+
+   					</ul>
+
+   				</div>
+
+   			  </div>
+
+         </div> */}
+
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,183 @@
+let resumeData = {
+    "name": "Mathis Neunzig",
+    "role": "Software Developer, University Lecturer and ABAP-Fanboy",
+    "linkedinId":"mathis-neunzig",
+    "roleDescription": "I love Databases, Software Engineering, Architectures and Data Science! My favorite programming languages are ABAP, Java, JS/TS and Erlang :)",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/mathis-neunzig/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/DrBackmischung",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"https://www.instagram.com/mathis_neunzig/",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am a passionate Software Developer and Teacher. I love data, code and cats 🐈!",
+    "address":"Heidelberg, Germany",
+    "website":"https://mathis-neunzig.de",
+    "education":[
+      {
+        "UniversityName":"DHBW Mannheim (Baden-Württemberg Cooperative State University Mannheim)",
+        "specialization":"B. Sc. Wirtschaftsinformatik (Software Engineering)",
+        "MonthOfPassing":"September",
+        "YearOfPassing":"2023",
+        "MonthOfStarting":"September",
+        "YearOfStarting":"2020",
+        "Achievements":"Bachelor of Science with 210 ECTS"
+      },
+      {
+        "UniversityName":"Fernuniversität Hagen (University Hagen)",
+        "specialization":"M. Sc. Data Science",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2026 (planned)",
+        "MonthOfStarting":"October",
+        "YearOfStarting":"2023",
+        "Achievements":"Master of Science with 120 ECTS"
+      }
+    ],
+    "certificates":[
+      {
+        "UniversityName":"Helsingin Yliopisto (University Helsinki)",
+        "specialization":"AI Ethics & AI in Society",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2024",
+        "MonthOfStarting":"February",
+        "YearOfStarting":"2024",
+        "Achievements":"5 ECTS"
+      },
+      {
+        "UniversityName":"Helsingin Yliopisto (University Helsinki)",
+        "specialization":"Functional Programming with Haskell",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2024",
+        "MonthOfStarting":"June",
+        "YearOfStarting":"2024",
+        "Achievements":"5 ECTS"
+      },
+      {
+        "UniversityName":"Universidad de Granada (University Granada)",
+        "specialization":"ML & Data Science for Bioinformatics",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2025",
+        "MonthOfStarting":"September",
+        "YearOfStarting":"2025",
+        "Achievements":"4 ECTS"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"SAP SE",
+        "specialization":"Vocational Training Student",
+        "MonthOfStarting":"September",
+        "YearOfStarting":"2020",
+        "MonthOfLeaving":"September",
+        "YearOfLeaving":"2023",
+        "Achievements":"I was a Vocational Training Student as part of SAP's Vocational Training/STAR Program. During this time, I had 6 different internships within SAP while completing my bachelors degree at DHBW Mannheim. I have spent most internships in software development, especially in ABAP development."
+      },
+      {
+        "CompanyName":"SAP SE",
+        "specialization":"Developer (ABAP & UI5)",
+        "MonthOfStarting":"September",
+        "YearOfStarting":"2023",
+        "Achievements":"I am an ABAP developer for key-user extensibility tools, like Custom Logic, Custom Business Objects or the XCO Library. "
+      }
+    ],
+    "teaching":[
+      {
+        "CompanyName":"DHBW Mannheim",
+        "specialization":"B. Sc. Wirtschaftsinformatik",
+        "MonthOfStarting":"February",
+        "YearOfStarting":"2024",
+        "Modules": [
+          {
+            "Name": "Software Engineering II",
+            "ECTS": 5
+          },
+          {
+            "Name": "Fortgeschrittene Systementwicklung",
+            "ECTS": 5
+          }
+        ]
+      },
+      {
+        "CompanyName":"DHBW Mosbach",
+        "specialization":"B. Sc. Computer Science (New Study)",
+        "MonthOfStarting":"September",
+        "YearOfStarting":"2024",
+        "Modules": [
+          {
+            "Name": "Database Systems",
+            "ECTS": 6
+          }
+        ]
+      },
+      {
+        "CompanyName":"DHBW Karlsruhe",
+        "specialization":"B. Sc. Wirtschaftsinformatik",
+        "MonthOfStarting":"August",
+        "YearOfStarting":"2024",
+        "MonthOfLeaving":"November",
+        "YearOfLeaving":"2024",
+        "Modules": [
+          {
+            "Name": "Software Engineering II",
+            "ECTS": 5
+          }
+        ]
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
